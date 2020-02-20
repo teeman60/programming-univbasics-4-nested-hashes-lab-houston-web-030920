71108627832f98486727484d248f370619a876b0
Taiye Salami <taiye41@gmail.com>
2020-02-20 15:34:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-return programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+return programmer_hash
 end
 
  
